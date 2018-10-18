1038aa72326967e2de6e24fd3143d3b9a12e6b25
Aaron Bjork <aaronbjork@hotmail.com>
2018-10-18 12:35:50 -0700

    Index update with proper welcome message.

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Edit <code>src/App.js</code> and save to reload. You made it!
+            Edit <code>src/App.js</code> and save to reload. You made it! Finally... !
           </p>
           <a
             className="App-link"
