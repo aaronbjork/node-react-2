cb51d373cd5b29cfaefe82a9ce1440e04b3d4d85
Aaron Bjork <aaronbjork@hotmail.com>
2018-10-18 12:53:24 -0700

    Updated welcome screen.

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Edit <code>src/App.js</code> and save to reload. You made it! Finally... !
+            Edit <code>src/App.js</code> New stuff goes here.
           </p>
           <a
             className="App-link"
